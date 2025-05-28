828d360d92c9ff92dd62cc9985e7009b9083e56e
LazzzyBlazer <lazzzyblazer@gmail.com>
2025-05-28 23:04:23 +0300

    0.0.1
    -movement
    -jump
    -wall jump
    -anim
    -world

---
+++ platformer_test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,110 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float speed;
+    [SerializeField] private float jump;
+    private Animator anim;
+    private BoxCollider2D BoxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
+
+    private void Awake()
+    {
+        //Components
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        BoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        //Get axis
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+
+        //Flip player sprite L/R
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        //Set animator
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", IsGroundet());
+
+
+        //Wall jump logic
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+            if (OnWall() && !IsGroundet())
+            {
+                body.gravityScale = 0;
+                body.linearVelocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 7;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        if (IsGroundet())
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
+            anim.SetTrigger("jump");
+        }
+        else if (OnWall() && !IsGroundet())
+        {
+            if(horizontalInput == 0)
+            {
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+            wallJumpCooldown = 0;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private bool IsGroundet()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(BoxCollider.bounds.center, BoxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool OnWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(BoxCollider.bounds.center, BoxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+}
